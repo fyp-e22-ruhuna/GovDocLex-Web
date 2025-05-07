e52d21539ffe3ddcd4ede8de0a01293dc774f436
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2025-05-07 16:46:16 +0530

    create navbar

---
+++ src/constants/navItems.constants.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+    { id: '', label: 'Home' },
+    { id: 'about', label: 'About' },
+    { id: 'features', label: 'Features' },
+    { id: 'explore', label: 'Explore' },
+    { id: 'team', label: 'Team' },
+    
+];
