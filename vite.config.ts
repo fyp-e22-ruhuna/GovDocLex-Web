220a440fad27807d45a96d9f313eae8ee9a20e00
dilshanka <ranaweera_kd_e22@engug.ruh.ac.lk>
2025-05-11 12:34:09 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+// import { defineConfig } from "vite"; 
+// import react from "@vitejs/plugin-react-swc"; 
+// import path from "path"; 
+ 
+// // https://vitejs.dev/config/ 
+// export default defineConfig({ 
+//   plugins: [react()], 
+//   resolve: { 
+//     alias: { 
+//       "@": path.resolve(__dirname, "src"), 
+//     }, 
+//   }, 
+// });
+
 import { defineConfig } from "vite"; 
 import react from "@vitejs/plugin-react-swc"; 
 import path from "path"; 
  
-// https://vitejs.dev/config/ 
 export default defineConfig({ 
+ 
   plugins: [react()], 
   resolve: { 
     alias: { 
