d2ac935686af2c993592d8ef7605618794c14861
dilshanka <ranaweera_kd_e22@engug.ruh.ac.lk>
2025-05-11 13:19:28 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path"; 
  
 export default defineConfig({ 
- 
+  base: '/GovDocLex-Web/', // <-- Add this line, replace with your repo name
   plugins: [react()], 
   resolve: { 
     alias: { 
